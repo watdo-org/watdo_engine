fd2a73b0faff6c5006b9d869da6028e046502f95
Nie Tsuki <nie.0x6708@gmail.com>
2025-08-19 01:10:22 +0800

    Implement apply_variables

--- src/variables.py
+++ src/variables.py
@@ -1,2 +1,7 @@
 def apply_variables(code: str, variables: dict[str, str]) -> str:
-    raise NotImplementedError
+    try:
+        new_code = code.format(**variables)
+    except KeyError as error:
+        raise ValueError(f"Variable {error} is undefined")
+
+    return new_code
