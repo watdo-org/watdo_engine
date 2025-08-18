cf2169cdd174c45a90a3765d8c86d673775d661f
Nie Tsuki <nie.0x6708@gmail.com>
2025-08-19 01:42:16 +0800

    Add block.py

---
+++ src/block.py
@@ -0,0 +1,9 @@
+from typing import TypedDict
+
+
+class PartialBlockData(TypedDict, total=False):
+    pass
+
+
+class BlockData(PartialBlockData):
+    pass
--- src/parser.py
+++ src/parser.py
@@ -1,12 +1,4 @@
-from typing import TypedDict
-
-
-class PartialBlockData(TypedDict, total=False):
-    pass
-
-
-class BlockData(PartialBlockData):
-    pass
+from .block import BlockData
 
 
 def parse_code(code: str) -> BlockData:
