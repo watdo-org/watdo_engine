ebd1fc2f9b2fa7998150a5101f321207c7bd21c0
Nie Tsuki <nie.0x6708@gmail.com>
2025-08-19 07:51:27 +0800

    Skip TestParseField

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,11 +1,14 @@
+import pytest
 from src.parser import parse_field, parse_code
 
 
 class TestParseField:
+    @pytest.mark.skip(reason="Not implemented yet")
     def test_parse_field(self) -> None:
-        raise NotImplementedError
+        pass
 
 
 class TestParseCode:
+    @pytest.mark.skip(reason="Not implemented yet")
     def test_parse_code(self) -> None:
-        raise NotImplementedError
+        pass
